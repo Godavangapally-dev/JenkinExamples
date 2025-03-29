3ca1bb887cbc50fc71b24329405880a8ff671e68
Godavangapally-dev <245122737011@mvsrec.edu.in>
2025-03-29 13:14:40 +0530

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -5,7 +5,7 @@ r1=n1+n2
 r2=n1-n2
 r3=n1*n2
 r4=n1/n2;
-print("Addition:"+r1)
-print("Subtraction:"+r2)
+print("Addition:"r1)
+print("Subtraction:"r2)
 print("Multiplication",r3)
 print("Division",r4)
