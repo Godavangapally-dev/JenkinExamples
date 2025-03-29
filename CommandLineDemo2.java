e5c66cc3b6d0cae888f3c344f65b3a8cdba87e26
Godavangapally-dev <245122737011@mvsrec.edu.in>
2025-03-29 12:41:20 +0530

    Add files via upload

--- CommandLineDemo2.java
+++ CommandLineDemo2.java
@@ -5,10 +5,10 @@ int rollno=Integer.parseInt(args[1]);
 int age=Integer.parseInt(args[2]);
 String branch=args[3];
 
-System.out.println("NAME OF STUDENT"+ name);
-System.out.println("ROLLNO"+ rollno);
-System.out.println("AGE"+ age);
-System.out.println("BRANCH"+ branch);
+System.out.println("NAME OF STUDENT:"+name);
+System.out.println("ROLLNO:"+rollno);
+System.out.println("AGE:"+age);
+System.out.println("BRANCH:"+branch);
 }
 }
 
